4c70fa4dff53d54fb07a4e3fbbe87dbd20b69865
Maxi Palavecino <108527937+MaxiPalavecino@users.noreply.github.com>
2022-08-05 11:44:29 -0300

    Create Datos.js

---
+++ Datos.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    name: "Cando",
+    planet: "Gliese-667Cc",
+    phrase: "«Si te caes siete veces, levántate ocho»",
+  },
+  {
+    name: "Cade",
+    planet: "Kepler-22b",
+    phrase: "«Sé el cambio que quieres ver en el mundo»",
+  },
+  {
+    name: "Deckard",
+    planet: "Kepler-69c",
+    phrase: "«Podemos sufrir muchas derrotas pero no debemos ser derrotados»",
+  },
+  {
+    name: "Bardan",
+    planet: "Kepler-62f",
+    phrase: "«No esperes. Nunca va a ser el momento adecuado»",
+  },
+  {
+    name: "Ax",
+    planet: "Kepler-186f",
+    phrase: "«La creatividad es la inteligencia divirtiéndose»",
+  },
+  {
+    name: "Galen",
+    planet: "Kepler-452b",
+    phrase: "«La energía y la persistencia conquistan todas las cosas»",
+  },
+  {
+    name: "Garrus",
+    planet: "Kepler-365aD",
+    phrase: "«La excelencia no es un acto, es un hábito»",
+  },
+  {
+    name: "Iriel",
+    planet: "Kepler-212tY",
+    phrase: "«Sé tú mismo. Todos los demás ya están ocupados»",
+  }
+]
